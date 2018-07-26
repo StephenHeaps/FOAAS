d0e3674a2332463e7a54cdc5424a63aa9f88126a
Stephen Heaps <steaps@me.com>
2018-07-26 19:15:53 -0400

    Initial Commit

---
+++ FOAAS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FOAAS
+//
+//  Created by Stephen Heaps on 2018-07-26.
+//  Copyright © 2018 Stephen Heaps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
